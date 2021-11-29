466ff64b740c6998dfa2bc97e841497219f7e34d
DESKTOP-1R07CSP\BestDeveloper <developer225@hotmail.com>
2021-11-29 12:11:29 +0900

    Home page icon animate button completed

--- src/components/AnimateButton/index.js
+++ src/components/AnimateButton/index.js
@@ -1,15 +1,27 @@
-import Blockie from 'components/Blockie';
+import { useState, useEffect } from 'react';
 import './style.scss';
 
 const AnimateButton = (props) => {
+    let [iconNum, setIconNum] = useState(0);
+    const iconGroup = [
+        <img src="../../assets/images/Star-1.png" alt="" />,
+        <img src="../../assets/images/heart.png" alt="" />,
+        <img src="../../assets/images/spade.png" alt="" />,
+        <img src="../../assets/images/diamond.png" alt="" />,
+        <img src="../../assets/images/club.png" alt="" />,
+    ];
+    let handleMouseEnter = () => {
+        let id = setInterval(() => {
+            setIconNum((prev) => {
+                if (prev >= 4) { clearInterval(id); setIconNum(0); }
+                return prev + 1;
+            });
+        }, 300);
+    };
     return (
-        <div className="btn_animate">
+        <div className="btn_animate" onMouseEnter={handleMouseEnter}>
             <div className="btn-icon-group">
-                <img src="../../assets/images/Star-1.png" style={{ display: "block" }} alt="" />
-                <img src="../../assets/images/heart.png" style={{ display: "none" }} alt="" />
-                <img src="../../assets/images/spade.png" style={{ display: "none" }} alt="" />
-                <img src="../../assets/images/diamond.png" style={{ display: "none" }} alt="" />
-                <img src="../../assets/images/club.png" style={{ display: "none" }} alt="" />
+                {iconGroup[iconNum]}
             </div>
             {props.children}
         </div>
