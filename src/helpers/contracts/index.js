5be647f9521b3e870b7f4706f80b662d50f7f791
holdemheroes <github@holdemheroes.com>
2022-02-18 15:58:45 +0000

    support for old price function

--- src/helpers/contracts/index.js
+++ src/helpers/contracts/index.js
@@ -1,9 +1,11 @@
 import holdemHeroesAbi from "./abis/HoldemHeroes.json"
 import texasHoldemV1 from "./abis/TexasHoldemV1.json"
+import holdemHeroesOldAbi from "./abis/HoldemHeroes(old).json"
 
 const abis = {
   heh_nft: holdemHeroesAbi,
   texas_holdem_v1: texasHoldemV1,
+  heh_old: holdemHeroesOldAbi,
 };
 
 export default abis;
