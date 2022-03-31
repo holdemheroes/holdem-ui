df6347469f57cb3752be721e4e5d415636fbbb19
holdemheroes <github@holdemheroes.com>
2022-03-31 16:11:55 +0100

    better provider error message parsing

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -30,31 +30,54 @@ export const openNotification = ({ message, description, type }) => {
 };
 
 export const extractErrorMessage = (e) => {
+
+  if(e?.code) {
+    if(Number.isInteger(e.code)) {
+      // already parsed (e.g. MetaMask's "user rejected tx"), return the original message
+      return e.message
+    }
+  }
+
+  // see if we can regex the error buried inside the message & stack
   const eStr = e.toString()
-  const matches = eStr.match(/"execution reverted: (.*?)"/);
-  const extracted = matches ? matches[1] : e.message
-  switch(extracted) {
-    case "eth too low":
-      return "Purchase price too low. Try a higher price"
-    case "not started":
-      return "Sale has not started yet"
-    case "sold out":
-      return "Sold out!"
-    case "ended":
-      return "Sale has ended"
-    case "> max per tx":
-      return "Cannot mint more than maximum"
-    case "> mint limit":
-      return "Blind mint limit reached for this wallet"
-    case "exceeds supply":
-      return "Cannot mint more than supply!"
-    case "not revealed":
-      return "Hands have not been revealed yet"
-    case "not distributed":
-      return "Hands have not been distributed yet"
-    case "invalid id":
-      return "Invalid Token ID"
-    default:
-      return extracted
+  const matches = eStr.match(/\(error=(.*?), method=/);
+
+  if(matches) {
+    try {
+      const errObj = JSON.parse(matches[1])
+      // if it's a contract revert, transform it into a more readable error message
+      switch(errObj.message) {
+        case "execution reverted: eth too low":
+          return "Purchase price too low. Try a higher price"
+        case "execution reverted: not started":
+          return "Sale has not started yet"
+        case "execution reverted: sold out":
+          return "Sold out!"
+        case "execution reverted: ended":
+          return "Sale has ended"
+        case "execution reverted: > max per tx":
+          return "Cannot mint more than maximum"
+        case "execution reverted: > mint limit":
+          return "Blind mint limit reached for this wallet"
+        case "execution reverted: exceeds supply":
+          return "Cannot mint more than supply!"
+        case "execution reverted: not revealed":
+          return "Hands have not been revealed yet"
+        case "execution reverted: not distributed":
+          return "Hands have not been distributed yet"
+        case "execution reverted: invalid id":
+          return "Invalid Token ID"
+        default:
+          // otherwise, just return the parsed message
+          return errObj.message
+      }
+    } catch(pErr) {
+      // couldn't turn into JSON. Just return original message
+      console.log(pErr)
+      return e.message
+    }
+  } else {
+    // just return the original message
+    return e.message
   }
 }
