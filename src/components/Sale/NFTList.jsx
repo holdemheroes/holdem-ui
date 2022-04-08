a7b0467ca2f457af263369289efeb489f1268185
holdemheroes <github@holdemheroes.com>
2022-04-08 16:59:34 +0100

    remove canMint

--- src/components/Sale/NFTList.jsx
+++ src/components/Sale/NFTList.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NFT from "./NFT";
 
-export default function NFTList({ currentTokens, canMint, mintedTokens, pricePerToken }) {
+export default function NFTList({ currentTokens, mintedTokens, pricePerToken }) {
 
   return (
     <div className="nft_list">
       {currentTokens &&
         currentTokens.map((item) => (
-          <NFT key={`nft_id_${item}`} tokenId={item} canMint={canMint} mintedTokens={mintedTokens} pricePerToken={pricePerToken} />
+          <NFT key={`nft_id_${item}`} tokenId={item} mintedTokens={mintedTokens} pricePerToken={pricePerToken} />
         ))}
     </div>
   );
