e367e544bc801aaa41bf4bec01586baea0848d88
holdemheroes <github@holdemheroes.com>
2022-04-13 14:21:32 +0100

    simplify sale chart for production

--- src/components/Sale/PriceEchart.jsx
+++ src/components/Sale/PriceEchart.jsx
@@ -1,47 +1,34 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react"
 import * as echarts from 'echarts'
 import Moralis from "moralis"
-import { useNFTSaleInfo } from "../../hooks/useNFTSaleInfo"
 import { Spin } from "antd"
 import { hehEchartsTheme } from './echarts_theme'
 
 export default function PriceEChart() {
 
-  const { targetEms } = useNFTSaleInfo();
-
   const [ chartData, setChartData ] = useState(null);
   const [ lastBlock, setLastBlock ] = useState(0);
   const [ data, setData ] = useState([]);
-  const [ ems, setEms ] = useState([]);
-  const [ targetEmsData, setTargetEmsData ] = useState([]);
   const [ labels, setLabels ] = useState([]);
 
   const processChartData = async () => {
     const params = { lastBlock: lastBlock };
     const cd = await Moralis.Cloud.run("getPricePerBlockData", params);
     const tmpData = data
-    const tmpEms = ems
-    const tmpTargetEms = targetEmsData
     const tmpLabels = labels
 
     for(let i = 0; i < cd.length; i += 1) {
       tmpLabels.push(Number(cd[i].blockTimestampMillis))
       tmpData.push(cd[i].price)
-      tmpEms.push(Number(cd[i].ems))
-      tmpTargetEms.push(Number(targetEms.toString()))
       if(parseInt(cd[i].block, 10) > lastBlock) {
         setLastBlock(cd[i].block)
       }
     }
     setLabels(tmpLabels)
     setData(tmpData)
-    setEms(tmpEms)
-    setTargetEmsData(tmpTargetEms)
     const d = {
       labels,
       price: data,
-      ems,
-      targetEms: targetEmsData,
     }
     setChartData(d)
   }
@@ -51,11 +38,11 @@ export default function PriceEChart() {
     async function _procData() {
       await processChartData()
     }
-    if (!chartData && targetEms) {
+    if (!chartData) {
       _procData()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chartData, targetEms]);
+  }, [chartData]);
 
   useEffect(() => {
     async function _procData() {
@@ -63,7 +50,7 @@ export default function PriceEChart() {
     }
 
     const timeout = setTimeout(() => {
-      if(chartData && targetEms) {
+      if(chartData) {
         _procData()
       }
     }, 15000);
@@ -72,7 +59,7 @@ export default function PriceEChart() {
       clearTimeout(timeout);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chartData, targetEms])
+  }, [chartData])
 
   if(!chartData) {
     return <Spin />
@@ -80,7 +67,7 @@ export default function PriceEChart() {
 
   return (
     <>
-      <Graph stats={chartData ? chartData : {labels: [], price: [], ems: [], targetEms: []}} />
+      <Graph stats={chartData ? chartData : {labels: [], price: []}} />
     </>
   );
 }
@@ -104,9 +91,6 @@ function Graph({ stats }) {
     if (chartInstance && stats) {
       chartInstance
         .setOption({
-          legend: {
-            show: true,
-          },
           xAxis: {
             type: 'category',
             data: stats.labels.map(item => new Date(item).toLocaleString()),
@@ -118,17 +102,7 @@ function Graph({ stats }) {
             {
               type: 'value',
               position: 'left',
-              name: 'ETH Price'
-            },
-            {
-              type: 'value',
-              position: 'right',
-              name: 'EMS',
-              axisLabel: {
-                formatter: function (value, index) {
-                  return (value / 1000000000000000000) + 'e18';
-                }
-              }
+              name: 'ETH Price per NFT'
             }
           ],
           series: [
@@ -138,20 +112,6 @@ function Graph({ stats }) {
               type: 'line',
               smooth: true,
               name: 'Price',
-            },
-            {
-              yAxisIndex: 1,
-              data: stats.ems,
-              type: 'line',
-              smooth: true,
-              name: 'EMS',
-            },
-            {
-              yAxisIndex: 1,
-              data: stats.targetEms,
-              type: 'line',
-              smooth: true,
-              name: 'Target EMS',
             }
           ]
         })
